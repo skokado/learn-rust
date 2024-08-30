544bb64dbec392fc3e687dbcd67c9dc33b81dad6
Shota Kokado <shota.kokado@revcomm.co.jp>
2024-08-30 19:00:36 +0900

    cleaned as rust-boy

---
+++ example.py
@@ -0,0 +1,20 @@
+class Node:
+    "TODO"
+
+
+class DoublyLinkedList:
+    "TODO"
+
+
+if __name__ == "__main__":
+    l = DoublyLinkedList()
+    l.push_back(1)
+    l.push_back(2)
+    l.push_back(3)
+
+    print(l)  # 1<--->2<--->3
+
+    l.push_front(4)
+    l.push_front(5)
+
+    print(l)  #  5<--->4<--->1<--->2<--->3
--- linked_list/python/__init__.py
+++
@@ -1,2 +0,0 @@
-from .linked_list import LinkedList
-from .node import Node
