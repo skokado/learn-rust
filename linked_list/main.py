3e6662cabe6d07c4d74fd7b898af8efb1cb130ce
skokado <terutacchi@gmail.com>
2024-08-31 19:47:38 +0900

    feat: impl python

--- linked_list/main.py
+++ linked_list/main.py
@@ -1,10 +1,65 @@
+from typing import Optional, TypeVar
+
+T = TypeVar("T")
+
+
 class Node:
-    "TODO"
+    def __init__(self, data: T, prev: Optional["Node"] = None, next: Optional["Node"] = None):
+        self.data = data
+        self.prev = prev
+        self.next = next
+
+    def __str__(self):
+        return f"Node({self.data})"
+
+    def __repr__(self):
+        return self.__str__()
 
 
 class DoublyLinkedList:
-    "TODO"
+    def __init__(self):
+        self.head = None
+        self.tail = None
 
+    def __str__(self):
+        if self.is_empty():
+            return
+        
+        node = self.head
+        res = str(node)
+        while node.next is not None:
+            node = node.next
+            res += f"<--->{node}"
+
+        return res
+
+    def __repr__(self) -> str:
+        return self.__str__()
+
+    def is_empty(self) -> bool:
+        return self.head is None
+    
+    def push_back(self, data: T):
+        new_node = Node(data)
+        if self.is_empty():
+            self.head = new_node
+            self.tail = new_node
+            return
+        
+        new_node.prev = self.head
+        self.tail.next = new_node
+        self.tail = new_node
+
+    def push_front(self, data: T):
+        new_node = Node(data)
+        if self.is_empty():
+            self.head = new_node
+            self.tail = new_node
+            return
+
+        new_node.next = self.head
+        self.head.prev = new_node
+        self.head = new_node
 
 if __name__ == "__main__":
     l = DoublyLinkedList()
@@ -14,6 +69,6 @@ if __name__ == "__main__":
     l.push_back(3)
     print(l)  # 1<--->2<--->3
 
-    l.push_back(4)
-    l.push_back(5)
+    l.push_front(4)
+    l.push_front(5)
     print(l)  # 5<--->4<--->1<--->2<--->3
