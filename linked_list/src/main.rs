f34abcc27791cdc6e0f71f9c8cbb4842b3f107f8
skokado <terutacchi@gmail.com>
2024-09-01 00:46:35 +0900

    feat: push_back の完成

--- linked_list/src/main.rs
+++ linked_list/src/main.rs
@@ -1,21 +1,17 @@
+use std::cell::RefCell;
 use std::fmt::{Display, Formatter, Result};
+use std::rc::Rc;
 
 #[derive(Clone)]
-struct Node<T> {
+struct Node<T: Clone> {
     data: T,
-    prev: Option<Box<Node<T>>>,
-    next: Option<Box<Node<T>>>,
+    next: Option<Rc<RefCell<Node<T>>>>,
+    prev: Option<Rc<RefCell<Node<T>>>>,
 }
 
-impl<T: Display> Display for Node<T> {
-    fn fmt(&self, f: &mut Formatter) -> Result {
-        write!(f, "Node({})", self.data)
-    }
-}
-
-struct DoublyLinkedList<T> {
-    head: Option<Box<Node<T>>>,
-    tail: Option<Box<Node<T>>>,
+struct DoublyLinkedList<T: Clone> {
+    head: Option<Rc<RefCell<Node<T>>>>,
+    tail: Option<Rc<RefCell<Node<T>>>>,
 }
 
 impl<T: Clone> DoublyLinkedList<T> {
@@ -27,18 +23,19 @@ impl<T: Clone> DoublyLinkedList<T> {
     }
 
     fn push_back(&mut self, data: T) {
-        let mut new_node = Box::new(Node {
-            data: data.clone(),
+        let new_node = Node {
+            data: data,
             prev: None,
             next: None,
-        });
+        };
+
+        let new_node = Rc::new(RefCell::new(new_node));
         if self.head.is_none() {
             self.head = Some(new_node.clone());
             self.tail = Some(new_node);
         } else {
-            new_node.prev = self.tail.clone();
             if let Some(tail) = &mut self.tail {
-                tail.next = Some(new_node.clone());
+                tail.borrow_mut().next = Some(new_node.clone());
             }
             self.tail = Some(new_node);
         }
@@ -63,15 +60,17 @@ where
 {
     fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
         let mut current = self.head.clone();
-        write!(f, "(")?;
+        write!(f, "[head] (")?;
+
         while let Some(node) = current {
-            write!(f, "{}", node.data)?;
-            current = node.next.clone();
+            let n = node.borrow();
+            write!(f, "{}", n.data)?;
+            current = n.next.clone();
             if current.is_some() {
                 write!(f, "<--->")?;
             }
         }
-        write!(f, ")")?;
+        write!(f, ") [tail]")?;
         Ok(())
     }
 }
@@ -82,9 +81,11 @@ fn main() {
 
     list.push_back(1);
     println!("{}", list);
-    list.push_back(2);
-    list.push_back(3);
 
+    list.push_back(2);
+    println!("{}", list);
+
+    list.push_back(3);
     println!("{}", list); // 1<--->2<--->3
 
     list.push_front(4);
